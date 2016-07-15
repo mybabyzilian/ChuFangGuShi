36129c6280e2371e4423174da3402ea429afd7a3
邹国华 <zgh07280510@126.com>
2016-07-15 09:13:15 +0800

    我自己建的class

---
+++ app/src/main/java/com/lanou/zouguohua/chufanggushi/ZghActivity.java
@@ -0,0 +1,18 @@
+package com.lanou.zouguohua.chufanggushi;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zouguohua on 16/7/15.
+ */
+public class ZghActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //自己的Aty
+
+    }
+}
